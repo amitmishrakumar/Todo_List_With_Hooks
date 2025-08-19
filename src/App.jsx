2c124e23c525632f1801e5a1416213f360b1e335
amitmishrakumar <amitmishrag2020@gmail.com>
2025-08-19 08:05:10 +0530

    counter App for Task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-function Home() { return <h2>🏠 Home Page</h2>; }
-function About() { return <h2>ℹ️ About Page</h2>; }
-function Contact() { return <h2>📞 Contact Page</h2>; }
+import React_Counter_App from './React_Counter_App'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <div>
+       <React_Counter_App/>
+        </div>
+    )
 }
 
